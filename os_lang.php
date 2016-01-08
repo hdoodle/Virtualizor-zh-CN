c5dc6e5b92197ae48856ede5894bd96fe80257e3
yang-er <670171961@qq.com>
2016-01-09 01:11:20 +0800

    翻译os lang

--- os_lang.php
+++ os_lang.php
@@ -26,56 +26,56 @@ if(!defined('VIRTUALIZOR')){
 
 }
 
-$l['not_selected'] = 'You did not select the OS';
-$l['invalid_os'] = 'The OS submitted is invalid';
-$l['no_new'] = 'You did not specify the new password.';
-$l['no_conf'] = 'The confirmation password is missing.';
-$l['no_match'] = 'The passwords you gave do not match';
-$l['error_reins'] = 'There was an error while reinstalling the OS';
-$l['err_changing'] = 'There were some errors while setting the password.';
-$l['err_log_osreinstall'] = 'You have exceeded your OS reinstall limit. Please contact your administrator';
+$l['not_selected'] = '您尚未选择系统';
+$l['invalid_os'] = '您提交的系统是无效的';
+$l['no_new'] = '您尚未输入新密码。';
+$l['no_conf'] = '您尚未输入确认密码。';
+$l['no_match'] = '您的确认密码与新密码不匹配。';
+$l['error_reins'] = '重新安装系统时出现了错误。';
+$l['err_changing'] = '设置密码时出现了错误。';
+$l['err_log_osreinstall'] = '您已经达到了重装系统限制。请联系管理员。';
 
-$l['<title>'] = 'Re-Install OS';
-$l['reins_os'] = 'Re-Install OS';
-$l['os_reinstall'] = 'Reinstall';
-$l['new_pass'] = 'New Password';
-$l['retype_pass'] = 'Retype Password';
-$l['done'] = 'The OS was reinstalled successfully';
-$l['reins_notify'] = 'Reinstalling the VPS Operating System...';
+$l['<title>'] = '重装系统';
+$l['reins_os'] = '重装系统';
+$l['os_reinstall'] = '重装';
+$l['new_pass'] = '新密码';
+$l['retype_pass'] = '确认密码';
+$l['done'] = '系统成功重新安装。';
+$l['reins_notify'] = '正在重新安装VPS系统……';
 
 // OS Info Languages
-$l['centos_line1'] = 'CentOS is an Enterprise-class Linux Distribution derived from sources freely provided to the public by a prominent North American Enterprise Linux vendor.';
-$l['centos_line2'] = 'CentOS conforms fully with the upstream vendors redistribution policy and aims to be 100% binary compatible. CentOS mainly changes packages to remove upstream vendor branding and artwork.';
-$l['centos_line3'] = 'CentOS is developed by a small but growing team of core developers. CentOS is free.';
+$l['centos_line1'] = 'CentOS是由著名的北美企业Linux供应商免费提供给公众的企业级Linux发行版。';
+$l['centos_line2'] = 'CentOS完全遵循上游厂商的发行标准，100%二进制兼容。它的修改主要是去除商标和艺术品。';
+$l['centos_line3'] = 'CentOS由一个小但仍在成长的团队的核心开发者开发，是免费的。';
 
-$l['fedora_line1'] = 'Fedora is a Linux-based operating system that showcases the latest in free and open source software. Fedora is always free for anyone to use, modify, and distribute.';
-$l['fedora_line2'] = 'It is built by people across the globe who work together as a community: the Fedora Project. The Fedora Project is open and anyone is welcome to join.';
-$l['fedora_line3'] = 'The Fedora Project is out front for you, leading the advancement of free, open software and content.';
+$l['fedora_line1'] = 'Fedora基于Linux，展示了最新的自由开源软件。Fedora允许任何人自由地使用、修改和分发。';
+$l['fedora_line2'] = '它由个人组成的全球性组织The Fedora Project构建。The Fedora Project是开放的，欢迎任何人加入。';
+$l['fedora_line3'] = 'Fedora项目始终处于前沿，引领着自由开源软件及内容的进步。';
 
-$l['suse_line1'] = 'The openSUSE project is a worldwide effort that promotes the use of Linux everywhere.';
-$l['suse_line2'] = 'openSUSE creates one of the world\'s best Linux distributions, working together in an open, transparent and friendly manner as part of the worldwide Free and Open Source Software community.';
-$l['suse_line3'] = 'The project is controlled by its community and relies on the contributions of individuals, working as testers, writers, translators, usability experts, artists and ambassadors or developers.';
-$l['suse_line4'] = 'openSUSE is a free and Linux-based operating system for your PC, Laptop or Server.';
+$l['suse_line1'] = 'The openSUSE project是全球性的努力，促进了Linux的使用范围延伸。';
+$l['suse_line2'] = 'openSUSE是世界上最好的Linux发行版之一，是全球自由开源软件社区的每个人公开、透明和友好的工作成果。';
+$l['suse_line3'] = '该项目由社区控制，依赖于个人的贡献：测试员、作家、翻译家、可用性专家、艺术家、大使或开发者。';
+$l['suse_line4'] = 'openSUSE是免费的个人电脑、服务器Linux操作系统。';
 
-$l['ubuntu_line1'] = 'Ubuntu is part of the Debian family of Linux operating systems, which has the largest pool of developer talent, with every package built by experts.';
-$l['ubuntu_line2'] = 'Canonical\'s rigorous release management, quality assurance, stress testing and product design enhances the quality of Ubuntu Server.';
-$l['ubuntu_line3'] = 'Super-fast and great-looking, Ubuntu is a secure, intuitive operating system that powers desktops, servers, netbooks and laptops. Ubuntu is, and always will be, absolutely free.';
+$l['ubuntu_line1'] = 'Ubuntu是Debian家族的Linux操作系统，它提供了大量有才的开发人才，囊括了各种由专家创建的软件包。';
+$l['ubuntu_line2'] = 'Canonical严谨的发布管理、质量保证、压力测试和产品设计提升了Ubuntu服务器的质量。';
+$l['ubuntu_line3'] = '急速又美观，Ubuntu是一个安全且直观的操作系统，它运行在台式机、服务器、上网本和笔记本电脑上。Ubuntu将永远完全免费。';
 
-$l['debian_line1'] = 'Debian is a free operating system (OS) for your computer. An operating system is the set of basic programs and utilities that make your computer run.';
-$l['debian_line2'] = 'Debian uses the Linux kernel (the core of an operating system), but most of the basic OS tools come from the GNU project; hence the name GNU/Linux.';
-$l['debian_line3'] = 'Debian GNU/Linux provides more than a pure OS: it comes with over 25000 packages, precompiled software bundled up in a nice format for easy installation on your machine.';
+$l['debian_line1'] = 'Debian是一个各种计算机使用免费的操作系统。它提供了一组基本程序和工具使您的电脑运行。';
+$l['debian_line2'] = 'Debian使用Linux内核，但大多数的基本系统工具都来自于GNU项目；因而得名GNU/Linux操作系统。';
+$l['debian_line3'] = 'Debian GNU/Linux提供的不仅仅是一个纯粹的操作系统，它还有超过25000份软件可供你在机器上安装。';
 
 // Scientific
-$l['scientific_line1'] = 'Scientific Linux is a Linux distribution produced by Fermi National Accelerator Laboratory.';
-$l['scientific_line2'] = 'It is a free and open source operating system based on Red Hat Enterprise Linux and aims to be \"as close to the commercial enterprise distribution as we can get it\".';
-$l['scientific_line3'] = 'This product is derived from the free and open source software made available by Red Hat, Inc., but is not produced, maintained or supported by Red Hat.';
-$l['conf_reins'] = 'Are you sure you want to re-install the OS ? All your data will be erased !';
-$l['strength_indicator'] = 'Strength Indicator';
-$l['bad'] = 'Bad';
-$l['good'] = 'Good';
-$l['strong'] = 'Strong';
-$l['short'] = 'Short';
-$l['weak_pass'] = 'Password must contain atleast 3 of the following : An uppercase letter, a lower case letter, a number or a special symbol';
-$l['pass_match'] = 'Passwords do not match';
+$l['scientific_line1'] = 'Scientific Linux是由Fermi National Accelerator Laboratory制作的 Linux 发行版。';
+$l['scientific_line2'] = '它是基于RedHat Enterprise的一个免费的开源操作系统，其目标是“尽可能达到商业级发行标准”。';
+$l['scientific_line3'] = '该产品是由RedHat所提供的免费开源软件衍生，但RedHat不提供生产、维护或支持。';
+$l['conf_reins'] = '你确定要重新安装操作系统？您的所有数据将被删除！';
+$l['strength_indicator'] = '强度指示';
+$l['bad'] = '坏';
+$l['good'] = '好';
+$l['strong'] = '强';
+$l['short'] = '短';
+$l['weak_pass'] = '密码至少包含以下三样：大写字母、小写字母、数字或特殊符号';
+$l['pass_match'] = '不匹配';
 
 ?>
