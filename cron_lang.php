8be526c51a4f018261106501b1a586c5927bfb63
yang-er <670171961@qq.com>
2016-01-09 18:03:46 +0800

    Update cron_lang.php

--- cron_lang.php
+++ cron_lang.php
@@ -26,69 +26,67 @@ if(!defined('VIRTUALIZOR')){
 
 }
 
-$l['upd_avail_sub'] = 'Virtualizor Upgrades v&soft-1; is available';
-$l['upd_avail'] = 'Virtualizor Upgrades v&soft-1; is available.
-Since \'Automatic Updates\' are OFF you will need to manually upgrade to the latest version.
-To do so go to:
-Admin Panel -> Configuration -> Updates
+$l['upd_avail_sub'] = 'Virtualizor升级v&soft-1;可用';
+$l['upd_avail'] = 'Virtualizor升级v&soft-1;可用。
+由于“自动升级”被关闭，你将需要手工升级到最新版本。
+要做到这一点，请访问：
+管理面板->配置->更新
 
-The following information about the new version was fetched:
+关于新版本以下信息：
 &soft-2;
 
-From Virtualizor Cron Jobs';
+来自Virtualizor计划作业';
 
-$l['upd_avail_suc_sub'] = 'Successfully Upgraded to Virtualizor v&soft-1;';
-$l['upd_avail_suc'] = 'Virtualizor Upgrades v&soft-1; is available.
-Virtualizor successfully Upgraded to latest version.
-Below is the logs of the Upgrade Attempt:
+$l['upd_avail_suc_sub'] = '成功升级到Virtualizor v&soft-1;';
+$l['upd_avail_suc'] = 'Virtualizor升级v&soft-1;可用。
+Virtualizor成功升级到最新版本。
+下面是升级尝试的日志：
 &soft-2;
 
-The following information about the new version was fetched:
+关于新版本以下信息：
 &soft-3;
 
-From Virtualizor Cron Jobs';
+来自Virtualizor计划作业';
 
-$l['upd_avail_err_sub'] = 'Failed Upgrading to Virtualizor v&soft-1;';
-$l['upd_avail_err'] = 'Virtualizor Upgrades v&soft-1; is available.
-Virtualizor tried to automatically upgrade to the latest version but failed.
-Below is the logs of the Upgrade Attempt:
+$l['upd_avail_err_sub'] = '失败升级到Virtualizor v&soft-1;';
+$l['upd_avail_err'] = 'Virtualizor升级v&soft-1;可用。
+Virtualizor尝试升级到最新版本但失败。
+下面是升级尝试的日志：
 &soft-2;
 
-You will have to manually upgrade to the latest version.
-To do so go to:
-Admin Panel -> Configuration -> Updates
+由于“自动升级”被关闭，你将需要手工升级到最新版本。
+要做到这一点，请访问：
+管理面板->配置->更新
 
-The following information about the new version was fetched:
+关于新版本以下信息：
 &soft-3;
 
-From Virtualizor Cron Jobs';
+来自Virtualizor计划作业';
 
 //Admin email limit alert
-$l['admin_mail_limit_sub'] = 'System Resources Usage Limit exceeded';
+$l['admin_mail_limit_sub'] = '超出系统资源的使用限制';
 $l['admin_mail_limit'] = '
 
-Server IP : {{ip}}
+服务器IP：{{ip}}
 
-Load : {{load}}
+负载：{{load}}
 
-CPU Usage : {{cpu_usage}} %
+CPU使用：{{cpu_usage}} %
 
-Ram Usage : {{ram_usage}} %
+内存使用：{{ram_usage}} %
 
-Disk Usage : {{disk_usage}} %
+磁盘使用：{{disk_usage}} %
 
-/tmp Usage : {{tmp_disk_usage}} %
+/tmp使用：{{tmp_disk_usage}} %
 
-Regards,
 {{sn}} {{ip}}';
 
 // Slave server status email
-$l['slave_offline_sub'] = 'Slave Server offline';
-$l['slave_offline'] = 'The following Slave server(s) are offline :
+$l['slave_offline_sub'] = '从属服务器离线';
+$l['slave_offline'] = '下面从属服务器处于离线：
 
 {{slave_text}}
 
-Regards,
 {{sn}} {{ip}}';
 
 
